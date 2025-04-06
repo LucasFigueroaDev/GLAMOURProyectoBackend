4343d0f140772c6495c4ae10eca83f65a2ecb27c
LucasFigueroa <lucasafigueroa93@gmail.com>
2025-04-06 16:41:17 -0300

    [M] Mongo

--- src/connections/mongo.js
+++ src/connections/mongo.js
@@ -5,8 +5,12 @@ dotenv.config();
 
 export const connectToMongo = async () => {
     try {
-        mongoose.connect(process.env.MONGO_KEY, { dbName: 'db-Project-CoderHouse-BackEnd-I' })
-            .then(() => console.log('Conexión a la base de datos exitosa')); 
+        await mongoose.connect(process.env.MONGO_KEY, {
+            dbName: 'db-Project-CoderHouse-BackEnd-I',
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000
+        })
+            .then(() => console.log('Conexión a la base de datos exitosa'));
     } catch (error) {
         console.log('Error al conectarse a la base de datos');
     }
